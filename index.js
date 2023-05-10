25ee08bd423a5c341e1e92446de980b87d05a2c1
Vaclav Ostrozlik <vaclav.ostrozlik@ataccama.com>
2023-05-10 12:48:16 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const cors = require("cors");
+const { parse, printSchema } = require("graphql");
+const { composeServices } = require("@apollo/composition");
+const {
+  compose,
+  signatureHeaderName,
+  verifyRequest,
+} = require("@graphql-hive/external-composition");
+
+const composeFederation = compose((services) => {
+  const result = composeServices(
+    services.map((service) => {
+      return {
+        typeDefs: parse(service.sdl),
+        name: service.name,
+        url: service.url,
+      };
+    })
+  );
+
+  if (result.errors?.length) {
+    return {
+      type: "failure",
+      result: {
+        errors: result.errors.map((error) => ({
+          message: error.message,
+          source:
+            typeof error.extensions?.code === "string"
+              ? "composition"
+              : "graphql",
+        })),
+      },
+    };
+  } else {
+    return {
+      type: "success",
+      result: {
+        supergraph: result.supergraphSdl,
+        sdl: printSchema(result.schema.toGraphQLJSSchema()),
+      },
+    };
+  }
+});
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.options("/api/compose", (req, res) => {
+  res.status(200).end();
+});
+
+app.post("/api/compose", (req, res) => {
+  const error = verifyRequest({
+    // Stringified body, or raw body if you have access to it
+    body: JSON.stringify(req.body),
+    // Pass here the signature from `X-Hive-Signature-256` header
+    signature: req.headers[signatureHeaderName],
+    // Pass here the secret you configured in Hive
+    secret: "__SECRET__",
+  });
+
+  if (error) {
+    // Failed to verify the request - send 500 and the error message back
+    res.status(500).send(error);
+  } else {
+    const result = composeFederation(req.body);
+    // Send the result back (as JSON)
+    res.send(JSON.stringify(result));
+  }
+});
+
+app.listen(3000, () => {
+  "App listening on port 3000";
+});
